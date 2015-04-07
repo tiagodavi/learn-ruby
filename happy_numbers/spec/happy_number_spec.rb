35ce2b98de7889607125d789e634984cf5b0c11c
sourish <hey@sourish.me>
2015-04-07 23:55:40 +0530

    updated code

--- happy_numbers/spec/happy_number_spec.rb
+++ happy_numbers/spec/happy_number_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper.rb'
 
 require 'happy_number.rb'
 
-describe HappyNumber do 
-	it "Is possibile to check if a number is happy?" do end
+describe HappyNumber do
+	it "Is possibile to check if a number is happy?" do
+	end
 end
