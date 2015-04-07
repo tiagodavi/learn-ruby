c8bc3e4dc6298358a5e608af8d207e249fc8ff53
Tiago Davi <tiago.asp.net@gmail.com>
2015-04-07 15:34:45 -0300

    Update happy_number_spec.rb

--- happy_numbers/spec/happy_number_spec.rb
+++ happy_numbers/spec/happy_number_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper.rb'
 require 'happy_number.rb'
 
 describe HappyNumber do
-	it "Is possibile to check if a number is happy?" do
-	end
+ it "Is possibile to check if a number is happy?" do
+  # Your test here
+ end
 end
